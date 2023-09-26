3c202adae586ed9769265191cc7f9168a09b4c3d
Marcos Diez <marcos@unitron.com.br>
2023-09-26 05:24:14 -0300

    blah

--- sync.sh
+++ sync.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 rsync -v -C ../atlantis run.sh secrets ubuntu@atlantis.xrd.com.br:
-ssh ubuntu@atlantis.xrd.com.br ./run.sh
+# ssh ubuntu@atlantis.xrd.com.br ./run.sh
